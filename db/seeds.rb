e322402323e07e60e9d8a018ad4bdd9bb0ea0f50
Charles Crane <charlie.crane46@gmail.com>
2020-11-12 12:15:27 +0000

    Models finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+data = JSON.parse(open(url).read)
+
+data.each { |i| puts i['strIngredient1'] if !data.first }
